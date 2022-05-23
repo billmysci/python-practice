d467062b727d4e455e8b632287d3f7616f095ec9
billmysci <68021713+billmysci@users.noreply.github.com>
2022-05-23 15:14:55 -0400

    Commit #2 from Kali

---
+++ ex3.1.py
@@ -0,0 +1,14 @@
+print("How much do you make a month after taxes?")
+
+print("Weekly salary after taxes", 125 * 0.92)
+print("Monthly salary after taxes", 125 * 0.92 * 4)
+
+print("How much can you save in a month?")
+
+print("Savings from work", (115-60) * 4)
+print("Wages from Hassler", (287.45 * 0.3) - 13.53)
+print("Wages from Tamara", ((300-(300 * 0.07)) * 0.4 - 10.96) * 2)
+
+print("How long will it take to save for a trip?")
+print("Cost of trip", 875 + 125 + 350)
+print("Time to save",(875 + 125 + 350) / (220 + 72 + 201))
--- ex3.py
+++ ex3.py
@@ -1,23 +1,31 @@
 print("I will now count my chickens.")
-
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+# counts the number of hens.
+print("Hens", 25.0 + 30.0 / 6.0)
+# counts the number of roosters.
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
 
 print("Now I will count the eggs:")
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# calculates the number of eggs.
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
-print(3 + 2 < 5 - 7)
+# evaluates if 3 + 2 is less than 5 - 7
+print(3.0 + 2.0 < 5.0 - 7.0)
 
+# adds 3 and 2
 print("What is 3 + 2?", 3 + 2)
-print("what is 5 + 7?", 5 + 7)
+# subtracts 7 from 5
+print("what is 5 - 7?", 5 - 7)
 
 print("Oh, that's why it's false.")
 
 print("How about some more.")
 
+# evaluates if 5 is greater than 5 - 7
 print("Is it greater?", 5 > -2)
+# evaluates if 5 is greater or equal to -2
 print("Is it greater or equal?", 5 >= -2)
+# evaluates if 5 is less or equal to -2
 print("Is it less or equal?", 5 <= -2)
